8316368d443074187be823e7545e6f45b59811ec
munira1212 <muni0301@stud.kea.dk>
2024-11-14 21:32:32 +0100

    Initial commit

---
+++ src/main/java/com/example/vrfsbackend/VrfsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vrfsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrfsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrfsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vrfsbackend/VrfsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vrfsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrfsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
